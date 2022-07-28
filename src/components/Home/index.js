c409f69b2ca7d1b38d6d963f92f527e6401713e6
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-07-27 21:10:41 -0400

    Main page structure

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,9 +2,94 @@ import './index.scss';
 
 const Home = () => {
     return (
-        <>
-            <h1>wow</h1>
-        </>
+        <div className="main">
+            <div className="header">
+                <h1>DIYAR'S BOOK REVIEWS</h1>
+            </div>
+            <div className="hot-now-section">
+                <h3>Hot Right Now</h3>
+                <p>Here's what Diyar's up to recently</p>
+                <div className="hot-now-items-container">
+                    Put images that have links to recent books I've reviewed
+                </div>
+            </div>
+
+            <div className="about-me">
+                <button className="about-me-button">
+                    Diyar who?
+                </button>
+            </div>
+            
+            <div className="search">
+
+            </div>
+
+            <div className="bookshelf-container">
+                <div className="bookshelf">
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                    <div className="book">
+                        <a href="/" className="book-cover">
+                            
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     );
 };
 
