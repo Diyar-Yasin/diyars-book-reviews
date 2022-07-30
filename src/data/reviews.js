a4574c0eee61b54270aac51dbf2d02814a152ff0
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-07-30 19:56:27 -0400

    Added books in previous website to database

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -1,6 +1,16 @@
-import TOKYO_GHOUL_RE from "./../assets/BookCovers/HotRightNow/TokyoGhoulRe.jpg";
-import COMPLETE_CTHULHU_MYTHOS_TALES from './../assets/BookCovers/HotRightNow/CompleteCthulhuMythosTales.jpg';
-import CHAINSAW_MAN from './../assets/BookCovers/HotRightNow/ChainsawMan.jpg';
+import TOKYO_GHOUL_RE from "./../assets/BookCovers/TokyoGhoulReCover.jpg";
+import COMPLETE_CTHULHU_MYTHOS_TALES from './../assets/BookCovers/TheCompleteCthulhuMythosTales.jpg';
+import CHAINSAW_MAN from './../assets/BookCovers/ChainSawManCover.jpg';
+import A_BRIEF_HISTORY_OF_TIME from './../assets/BookCovers/ABriefHistoryOfTime.jpg';
+import AMERICAN_PSYCHO from './../assets/BookCovers/AmericanPsycho.jpg';
+import BECOMING from './../assets/BookCovers/Becoming.jpg';
+import BORN_A_CRIME from './../assets/BookCovers/BornACrime.jpg';
+import I_AM_LEGEND from './../assets/BookCovers/IAmLegend.jpg';
+import NO_LONGER_HUMAN from './../assets/BookCovers/NoLongerHuman.jpg';
+import THE_ODYSSEY from './../assets/BookCovers/TheOdyssey.jpg';
+import THE_SILENT_PATIENT from './../assets/BookCovers/TheSilentPatient.jpg';
+import THE_SUBTLE_ART_OF_NOT_GIVING_A_FUCK from './../assets/BookCovers/TheSubtleArtOfNotGivingAFuck.jpg';
+import UNCLE_TOMS_CABIN from './../assets/BookCovers/UncleTom\'sCabin.jpg';
 
 export const Reviews = [
   {
@@ -31,6 +41,106 @@ export const Reviews = [
     "genres": "Horror fiction",
     "images": {
       "main": CHAINSAW_MAN
+    }
+  },
+  {
+    "key": "-a-brief-history-of-time",
+    "name": "A Brief History of Time",
+    "author": "Stephen Hawking",
+    "rating": 4,
+    "genres": "Popular science",
+    "images": {
+      "main": A_BRIEF_HISTORY_OF_TIME
+    }
+  },
+  {
+    "key": "-american-psycho",
+    "name": "American Psycho",
+    "author": "Bret Easton Ellis",
+    "rating": 1,
+    "genres": "Satire, Horror fiction, Postmodern literature, Dark comedy, Psychological Fiction, Transgressive fiction",
+    "images": {
+      "main": AMERICAN_PSYCHO
+    }
+  },
+  {
+    "key": "-becoming",
+    "name": "Becoming",
+    "author": "Michelle Obama",
+    "rating": 3,
+    "genres": "Memoir",
+    "images": {
+      "main": BECOMING
+    }
+  },
+  {
+    "key": "-born-a-crime",
+    "name": "Born a Crime",
+    "author": "Trevor Noah",
+    "rating": 4,
+    "genres": "Autobiography",
+    "images": {
+      "main": BORN_A_CRIME
+    }
+  },
+  {
+    "key": "-i-am-legend",
+    "name": "I Am Legend",
+    "author": "Richard Matheson",
+    "rating": 3,
+    "genres": "Horror",
+    "images": {
+      "main": I_AM_LEGEND
+    }
+  },
+  {
+    "key": "-no-longer-human",
+    "name": "No Longer Human",
+    "author": "Osamu Dazai",
+    "rating": 5,
+    "genres": "Fiction",
+    "images": {
+      "main": NO_LONGER_HUMAN
+    }
+  },
+  {
+    "key": "-the-odyssey",
+    "name": "The Odyssey",
+    "author": "Homer",
+    "rating": 4,
+    "genres": "Epic poetry",
+    "images": {
+      "main": THE_ODYSSEY
+    }
+  },
+  {
+    "key": "-the-silent-patient",
+    "name": "The Silent Patient",
+    "author": "Alex Michaelides",
+    "rating": 3,
+    "genres": "Thriller, Novel, Mystery, Psychological thriller, Suspense, Psychological Fiction",
+    "images": {
+      "main": THE_SILENT_PATIENT
+    }
+  },
+  {
+    "key": "-the-subtle-art-of-not-giving-a-fuck",
+    "name": "The Subtle Art of Not Giving a Fuck",
+    "author": "Mark Manson",
+    "rating": 2,
+    "genres": "Self-help",
+    "images": {
+      "main": THE_SUBTLE_ART_OF_NOT_GIVING_A_FUCK
+    }
+  },
+  {
+    "key": "-uncle-tom's-cabin",
+    "name": "Uncle Tom's Cabin",
+    "author": "Harriet Beecher Stowe",
+    "rating": 4,
+    "genres": "Novel",
+    "images": {
+      "main": UNCLE_TOMS_CABIN
     }
   }
 ];
