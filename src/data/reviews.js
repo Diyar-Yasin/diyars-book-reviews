a8cfcc548ec8c18944043ebbecf9b17245c0883a
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-07-31 01:52:57 -0400

    Added more books to be reviewed

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -11,6 +11,22 @@ import THE_ODYSSEY from './../assets/BookCovers/TheOdyssey.jpg';
 import THE_SILENT_PATIENT from './../assets/BookCovers/TheSilentPatient.jpg';
 import THE_SUBTLE_ART_OF_NOT_GIVING_A_FUCK from './../assets/BookCovers/TheSubtleArtOfNotGivingAFuck.jpg';
 import UNCLE_TOMS_CABIN from './../assets/BookCovers/UncleTom\'sCabin.jpg';
+import A_WILD_SHEEP_CHASE from './../assets/BookCovers/A Wild Sheep Chase.jpg';
+import CIRCE from './../assets/BookCovers/Circe.jpg';
+import EXHALATION from './../assets/BookCovers/Exhalation.jpg';
+import KLARA_AND_THE_SUN from './../assets/BookCovers/KlaraAndTheSun.png';
+import THE_REMAINS_OF_THE_DAY from './../assets/BookCovers/TheRemainsOfTheDay.jpg';
+import LIFE_FOR_SALE from './../assets/BookCovers/LifeForSale.jpg';
+import ONE_BRIGHT_MOON from './../assets/BookCovers/OneBrightMoon.jpg';
+import THE_WIND_UP_BIRD_CHRONICLE from './../assets/BookCovers/TheWindUpBirdChronicle.jpg';
+import IN_ORDER_TO_LIVE from './../assets/BookCovers/InOrderToLive.jpg';
+import THE_SONG_OF_ACHILLES from './../assets/BookCovers/TheSongOfAchilles.jpg';
+import WHY_WE_SLEEP from './../assets/BookCovers/WhyWeSleep.jpg';
+import Q_N_A from './../assets/BookCovers/Q&A.jpg';
+import SPRING_SNOW from './../assets/BookCovers/SpringSnow.jpg';
+import RUNAWAY_HORSES from './../assets/BookCovers/RunawayHorses.jpg';
+import THE_TEMPLE_OF_DAWN from './../assets/BookCovers/TheTempleOfDawn.jpg';
+import THE_DECAY_OF_THE_ANGEL from './../assets/BookCovers/TheDecayOfTheAngel.jpg';
 
 export const Reviews = [
   {
@@ -141,6 +157,166 @@ export const Reviews = [
     "genres": "Novel",
     "images": {
       "main": UNCLE_TOMS_CABIN
+    }
+  },
+  {
+    "key": "-a-wild-sheep-chase",
+    "name": "A Wild Sheep Chase",
+    "author": "Haruki Murakami",
+    "rating": 3,
+    "genres": "Surreal novel, magical realism",
+    "images": {
+      "main": A_WILD_SHEEP_CHASE
+    }
+  },
+  {
+    "key": "-circe",
+    "name": "Circe",
+    "author": "Madeline Miller",
+    "rating": 4,
+    "genres": "Historical Fiction, Fantasy Fiction",
+    "images": {
+      "main": CIRCE
+    }
+  },
+  {
+    "key": "-exhalation",
+    "name": "Exhalation",
+    "author": "Ted Chiang",
+    "rating": 4,
+    "genres": "Science fiction, Metaphysical fiction",
+    "images": {
+      "main": EXHALATION
+    }
+  },
+  {
+    "key": "-in-order-to-live",
+    "name": "In Order To Live",
+    "author": "Yeonmi Park",
+    "rating": 3,
+    "genres": "Biography, Autobiography",
+    "images": {
+      "main": IN_ORDER_TO_LIVE
+    }
+  },
+  {
+    "key": "-klara-and-the-sun",
+    "name": "Klara and the Sun",
+    "author": "Kazuo Ishiguro",
+    "rating": 3,
+    "genres": "Science fiction, Dystopian",
+    "images": {
+      "main": KLARA_AND_THE_SUN
+    }
+  },
+  {
+    "key": "-the-remains-of-the-day",
+    "name": "The Remains of the Day",
+    "author": "Kazuo Ishiguro",
+    "rating": 3,
+    "genres": "Historical novel",
+    "images": {
+      "main": THE_REMAINS_OF_THE_DAY
+    }
+  },
+  {
+    "key": "-life-for-sale",
+    "name": "Life for Sale",
+    "author": "Yukio Mishima",
+    "rating": 3,
+    "genres": "Dark comedy, Satire",
+    "images": {
+      "main": LIFE_FOR_SALE
+    }
+  },
+  {
+    "key": "-one-bright-moon",
+    "name": "One Bright Moon",
+    "author": "Andrew Kwong",
+    "rating": 3,
+    "genres": "Biography, Autobiography",
+    "images": {
+      "main": ONE_BRIGHT_MOON
+    }
+  },
+  {
+    "key": "-the-wind-up-bird-chronicle",
+    "name": "The Wind-Up Bird Chronicle",
+    "author": "Haruki Murakami",
+    "rating": 3,
+    "genres": "Novel, Science fiction, Psychological Fiction, Fantastique",
+    "images": {
+      "main": THE_WIND_UP_BIRD_CHRONICLE
+    }
+  },
+  {
+    "key": "-the-song-of-achilles",
+    "name": "The Song of Achilles",
+    "author": "Madeline Miller",
+    "rating": 4,
+    "genres": "Novel, Romance novel, Historical Fiction",
+    "images": {
+      "main": THE_SONG_OF_ACHILLES
+    }
+  },
+  {
+    "key": "-why-we-sleep",
+    "name": "Why We Sleep",
+    "author": "Matthew Walker",
+    "rating": 4,
+    "genres": "Science book",
+    "images": {
+      "main": WHY_WE_SLEEP
+    }
+  },
+  {
+    "key": "-q_n_a",
+    "name": "Q&A",
+    "author": "Vikas Swarup",
+    "rating": 4,
+    "genres": "Drama",
+    "images": {
+      "main": Q_N_A
+    }
+  },
+  {
+    "key": "-spring-snow",
+    "name": "Spring Snow",
+    "author": "Yukio Mishima",
+    "rating": 3,
+    "genres": "Fiction",
+    "images": {
+      "main": SPRING_SNOW
+    }
+  },
+  {
+    "key": "-runaway-horses",
+    "name": "Runaway Horses",
+    "author": "Yukio Mishima",
+    "rating": 3,
+    "genres": "Fiction",
+    "images": {
+      "main": RUNAWAY_HORSES
+    }
+  },
+  {
+    "key": "-the-temple-of-dawn",
+    "name": "The Temple of Dawn",
+    "author": "Yukio Mishima",
+    "rating": 3,
+    "genres": "Fiction",
+    "images": {
+      "main": THE_TEMPLE_OF_DAWN
+    }
+  },
+  {
+    "key": "-the-decay-of-the-angel",
+    "name": "The Decay of the Angel",
+    "author": "Yukio Mishima",
+    "rating": 3,
+    "genres": "Fiction",
+    "images": {
+      "main": THE_DECAY_OF_THE_ANGEL
     }
   }
 ];
