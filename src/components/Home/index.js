bc22504af34af1c1de03517bb3f31e52bbb1141e
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-08-06 19:42:54 -0400

    searchbar now filters the physical books on screen instead of coming up with a list of options. Also fixed page visual bugs related to bookshelf

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,43 @@
 import './index.scss';
 import HotNowSection from './../HotNowSection';
 import AboutMeSection from './../AboutMeSection';
-import Searchbar from './../Searchbar';
 import { Reviews } from "./../../data/reviews";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     const navigate = useNavigate();
+    const [userInput, setUserInput] = useState("");
+    const [searchResultsExist, setSearchResultsExist] = useState(false);
+
+    const getMatchedResults = () => {
+        return Reviews.filter((item) => {
+            const searchTerm = userInput.toLowerCase();
+            const fullName = item.name.toLowerCase();
+
+            return (
+                searchTerm &&
+                fullName.includes(searchTerm)
+        )});
+    }
+
+    const handleChange = (text) => {
+        setUserInput(text);
+        const matchedResults = Reviews.filter((item) => {
+            const searchTerm = text.toLowerCase();
+            const fullName = item.name.toLowerCase();
+
+            return (
+                searchTerm.length > 0 &&
+                fullName.includes(searchTerm)
+        )});
+
+        if (matchedResults.length > 0) {
+            setSearchResultsExist(true);
+        } else {
+            setSearchResultsExist(false);
+        }
+    }
 
     return (
         <div className="main">
@@ -16,11 +47,25 @@ const Home = () => {
             
             <HotNowSection/>
             <AboutMeSection/>
-            <Searchbar/>
+            
+            <div className="search">
+                <div className="search-inner">
+                    <input className={searchResultsExist ? "search-text-box-results" : "search-text-box-no-results"} value={userInput} placeholder="Search for books by title" onChange={e => (handleChange(e.target.value))}/>
+                </div>
+            </div>
 
             <div className="bookshelf-container">
                 <div className="bookshelf">
                     {
+                        userInput.length > 0 ?
+                        getMatchedResults().map((item, i) => (    
+                            <div className="book" onClick={() => navigate("/bookreviewwebsite-v2/reviews" + item.key, { state: { id: 1, reviewData: item }})}>
+                                <img className="book-cover" src={item.images.main} alt={item.name}/>
+                            </div>
+                        ))
+
+                        :
+
                         Reviews.map((item, i) => (    
                             <div className="book" onClick={() => navigate("/bookreviewwebsite-v2/reviews" + item.key, { state: { id: 1, reviewData: item }})}>
                                 <img className="book-cover" src={item.images.main} alt={item.name}/>
