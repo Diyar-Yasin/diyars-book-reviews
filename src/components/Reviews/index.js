0f9e75b33a1e84db197178d61bea617fbe275b94
Diyar-Yasin <diyaryasin.dy@gmail.com>
2025-02-09 16:08:08 -0500

    removed back button, disabled cover animation in review

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -1,20 +1,17 @@
 import './index.scss';
-import { useLocation, useNavigate } from 'react-router-dom'; 
+import { useLocation } from 'react-router-dom'; 
 import InfoBox from './../InfoBox';
 
 const Reviews = () => {
-    const navigate = useNavigate();
     const location = useLocation();
     const reviewData = location.state.reviewData;
 
     return (
         <div className="review">
             <div className="heading">
-                <span class="fa fa-toggle-left back-button" onClick={() => navigate("/diyars-book-reviews")}/>
                 <h1>{reviewData.name}</h1>
             </div>
             <div className="review-content"> 
-                <div className="mobile-only-div">nice find!</div>
                 <InfoBox reviewData={reviewData}/>
                 <img alt="Review" className="review-image" src={reviewData.images.main} />
                 <div></div>
